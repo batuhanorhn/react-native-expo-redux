9677fdba1ebf8ce2c147ad7c6f2528ec072bcf76
Batuhan Orhan <batuhanorhan44@gmail.com>
2019-07-13 01:19:59 +0300

    fixed

--- source/reduxStore/index.js
+++ source/reduxStore/index.js
@@ -3,19 +3,11 @@ import { createLogger } from 'redux-logger'
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 
-// const mappers = new Mappers();
 const middleware = [ thunk ];
 
 if (process.env.NODE_ENV === 'development') {
   middleware.push(createLogger())
 }
-
-// middleware.push((store)=>(next)=>(action)=>{
-//   const prevState = store.getState();
-//   // next(mappers.bindToModel(action));
-//   const nextState = store.getState();
-//   push(action.type, nextState, prevState);
-// })
 
 const store = createStore(
   reducers,
